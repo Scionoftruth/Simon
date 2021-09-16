8ed61cabce8a5d7710ddb39c910513555610b160
Scionoftruth <charlieallen132@hotmail.com>
2021-09-16 09:45:57 -0500

    Update app-routing.module.ts

--- simon/src/app/app-routing.module.ts
+++ simon/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { HeadingComponent } from './heading/heading.component';
+import { GameoverComponent } from './gameover/gameover.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'/game', pathMatch:'full'},
-  { path: 'game', component: GameComponent}
+  { path: 'game', component: GameComponent},
+  { path: 'gameover', component: GameoverComponent}
 ];
 
 @NgModule({
