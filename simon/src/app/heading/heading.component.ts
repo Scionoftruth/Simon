fed2fec239d9f490259fe3664cc5865d42768ae1
rss32 <write_me_later@hotmail.com>
2021-09-16 20:35:12 -0400

    added css

--- simon/src/app/heading/heading.component.ts
+++ simon/src/app/heading/heading.component.ts
@@ -1,19 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-heading',
   templateUrl: './heading.component.html',
   styleUrls: ['./heading.component.css']
 })
-export class HeadingComponent implements OnInit {
+export class HeadingComponent implements OnInit,OnChanges {
 
     
   @Input()level:number = 0;
-  @Input()isGameOver:boolean = true;
+  @Input()isGameOver:boolean = false;
+
+  //displayed to the user based on how many rounds they play
+  firstLevelAdjective:string[] = ['GOOD','COOL','NICE','COMMENDABLE','GREAT'];
+  secondLevelAdjective:string[] = ['WONDERFUL','SUBLIME','DELIGHTFUL','SPLENDID','AMAZING'];
+  thirdLevelAdjective:string[] = ['MARVELOUS','GLORIOUS','EXQUISITE','EXCEPTIONAL','PHENOMINAL','ASTONISHING'];
+
+  adjBackground:string = '';
+
+  adjective:string="";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges():void{
+    this.setAdjective();
+    setTimeout(()=>this.resetAdjective(),3000);
+  }
+
+  setAdjective():void{
+    
+    if (this.level >=5 && this.level <=10 && !this.isGameOver){
+      this.adjBackground = 'first';
+      this.adjective = this.firstLevelAdjective[Math.floor(this.firstLevelAdjective.length * Math.random())];
+
+    }
+    if (this.level >10 && this.level <=20 && !this.isGameOver){
+      this.adjBackground = 'second';
+      this.adjective = this.secondLevelAdjective[Math.floor(this.secondLevelAdjective.length * Math.random())];
+    }
+    if (this.level >20 && !this.isGameOver){
+      this.adjBackground = 'third';
+      this.adjective = this.thirdLevelAdjective[Math.floor(this.thirdLevelAdjective.length * Math.random())];
+    }
+  }
+  resetAdjective():void{
+    this.adjective="";
   }
 
 }
